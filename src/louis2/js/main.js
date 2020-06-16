a4cad248ab9c502c07929c1ab06156f130f58c2c
icncsx <icncsx@gmail.com>
2020-06-16 01:36:11 -0400

    remove redundant js

--- src/louis2/js/main.js
+++
@@ -1,7 +0,0 @@
-var div = document.createElement("div");
-div.style.display = "grid";
-if (!(div.style.display === "grid")) {
-    alert(
-        "You are using a browser that does not support CSS grid! Please consider upgrading."
-    );
-}
